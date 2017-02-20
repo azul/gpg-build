514d5560b2c03a32e4ba68c31a38ca08d196beff
Azul <azul@riseup.net>
2017-02-20 19:56:09 +0100

    travis: GPG_VERSION env to configure

--- build-gpg.sh
+++ build-gpg.sh
@@ -2,9 +2,9 @@ PATH="$HOME/bin:$PATH"
 LD_LIBRARY_PATH="$HOME/lib:${LD_LIBRARY_PATH}"
 export PATH LD_LIBRARY_PATH
 
-wget https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.1.18.tar.bz2
-bzcat gnupg-2.1.18.tar.bz2 | tar xf -
-cd gnupg-2.1.18
+wget https://gnupg.org/ftp/gcrypt/gnupg/gnupg-$GPG_VERSION.tar.bz2
+bzcat gnupg-$GPG_VERSION.tar.bz2 | tar xf -
+cd gnupg-$GPG_VERSION
 make -f build-aux/speedo.mk native INSTALL_PREFIX="$HOME"
 
-gpg2 --version
+gpg2 --version | grep "gpg (GnuPG) $GPG_VERSION"
